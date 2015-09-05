96f582eec417bbefcfc43cbb311bfa81d0a4b3d2
Chaoyi Zha <summermontreal@gmail.com>
2015-09-04 23:44:27 -0400

    add nat setup

---
+++ linux_client/nat_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# set up NAT to route traffic through radio
+echo 1 > /proc/sys/net/ipv4/ip_forward
+/sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+/sbin/iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
