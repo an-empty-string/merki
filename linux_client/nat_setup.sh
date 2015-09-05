a55bd36e6690cacb438692798fc59e7395dd33d9
Chaoyi Zha <summermontreal@gmail.com>
2015-09-04 23:47:47 -0400

    Add dnsmasq

--- linux_client/nat_setup.sh
+++ linux_client/nat_setup.sh
@@ -5,3 +5,6 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 /sbin/iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
 /sbin/iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+
+# set up dnsmasq
+mv dnsmasq-merki.conf /etc/dnsmasq.d/98-merki.conf
